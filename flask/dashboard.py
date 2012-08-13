ae9dd05e19d9809d1dc34a4d871c4092610afa3d
Michael Lawrence Evans <mevans@stamen.com>
2012-08-12 23:41:23 -0700

    Adding new stats endpoints; adding new endpoints to get data in csv

--- flask/dashboard.py
+++ flask/dashboard.py
@@ -1,5 +1,4 @@
 import psycopg2
-
 import psycopg2.extras
 
 from flask import Flask, g
@@ -35,20 +34,45 @@ def parse_date(date_str, date_format='%Y-%m-%d'):
         return datetime.strptime(date_str, date_format)
     except Exception, e:
         return None
+        
+def query_db_lite(query, args=()):
+        #dbutils pylons
+        #cur = g.db.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+        cur = g.db.cursor()
+        try:
+                cur.execute(query, args)
+
+                #query_str = cur.mogrify(query,args)
+                #print query_str
+        except psycopg2.DataError,err:
+                print 'Error', err
+
+        #print 'description', cur.description
+        
+        try:
+                res = cur.fetchall() # fetching everything, rename the method?
+        except psycopg2.ProgrammingError, err:
+                res = None
+                
+                print 'Error', err
+
+        cur.close() # is this necessary?
+
+        return res
 
 def query_db(query, args=()):
         #dbutils pylons
         cur = g.db.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
-
+        
         try:
                 cur.execute(query, args)
 
-                query_str = cur.mogrify(query,args)
-                print query_str
+                #query_str = cur.mogrify(query,args)
+                #print query_str
         except psycopg2.DataError,err:
                 print 'Error', err
 
-        print 'description', cur.description
+        #print 'description', cur.description
         
         try:
                 res = cur.fetchall() # fetching everything, rename the method?
@@ -235,9 +259,6 @@ def types(type=None, service_code=None, count=None, total_count=None, weekly_cou
             elif daily_count_res[i-1]['date'] != daily_count_res[i]['date']:
                 daily_count_list.append({'date': daily_count_res[i]['date'], daily_count_res[i]['status']:daily_count_res[i]['count']})
 
-
-        #print daily_count_list
-
         daily_count_json = json.dumps(daily_count_list)
 
         requests_json = json.dumps(results)
@@ -256,17 +277,37 @@ def types(type=None, service_code=None, count=None, total_count=None, weekly_cou
 def daily_count(count=None):
     days = request.args.get("days", 60)
     
+    #end_date = datetime.now()
+    
+    res = query_db("""
+        SELECT MAX(requested_datetime) AS max_date
+        FROM sf_requests
+    """)
+    
+    #print 'latest:', res
+    
+    end_date = res[0]['max_date']
+    
+    start_date = end_date - timedelta(days=int(days))
+    
+    fmt_start_date = datetime.strftime(start_date, '%Y-%m-%d')
+    
+    fmt_end_date = datetime.strftime(end_date, '%Y-%m-%d')
+    
     daily_count_res = query_db("""
         SELECT 
             CAST(DATE(requested_datetime) as text), COUNT(*), status 
         FROM sf_requests 
-        WHERE requested_datetime BETWEEN NOW() - INTERVAL '(%s) DAY' AND NOW() 
+        WHERE DATE(requested_datetime) BETWEEN (%s) AND (%s)
         GROUP BY DATE(requested_datetime), status 
         ORDER BY DATE(requested_datetime) ASC
-    """, (days,))
+    """, (fmt_start_date, fmt_end_date))
 
-    #print daily_count_res
+    print daily_count_res
+    #print start_date, end_date
+    print fmt_start_date, fmt_end_date
 
+    """
     daily_count_list = []
 
     for i,j in enumerate(daily_count_res):
@@ -279,15 +320,61 @@ def daily_count(count=None):
             continue
 
     #print daily_count_list
-
-    return json.dumps(daily_count_list)
+    """
+    
+    input_exhausted = False
+    
+    date = start_date
+    
+    count = 0
+    
+    input = daily_count_res[count]
+    
+    next_date = start_date
+    
+    results = []
+    
+    while date <= end_date:
+        info = {}
+        
+        info['date'] = datetime.strftime(date, '%Y-%m-%d')
+        info['Open'] = 0
+        info['Closed'] = 0
+        
+        while not input_exhausted and info['date'] == input['date']:
+            if input['status'] == 'Closed':
+                info['Closed'] += input['count']
+            elif input['status'] == 'Open':
+                info['Open'] += input['count']
+                
+            count = count + 1
+            
+            if count >= len(daily_count_res):
+                input_exhausted = True
+            else:
+                input = daily_count_res[count]
+        
+        date = date + timedelta(1)
+        
+        results.append(info)
+    
+    return json.dumps(results)
     
 @app.route("/daily_count_by_neighborhood")
 def daily_count_by_neighborhood(count=None):
     days = request.args.get("days", 60)
     neighborhood = request.args.get("neighborhood")
     
-    end_date = datetime.now()
+    #end_date = datetime.now()
+    
+    res = query_db("""
+        SELECT MAX(requested_datetime) AS max_date
+        FROM sf_requests
+    """)
+    
+    #print 'latest:', res
+    
+    end_date = res[0]['max_date']
     
     start_date = end_date - timedelta(days=int(days))
     
@@ -301,7 +388,7 @@ def daily_count_by_neighborhood(count=None):
         FROM sf_requests as r
         JOIN pn_geoms as p
         ON ST_INTERSECTS(geom, ST_GeomFromText('POINT(' || r.lon || ' ' || r.lat || ')'))
-        WHERE p.neighborho=(%s) AND requested_datetime BETWEEN DATE((%s)) AND DATE((%s))
+        WHERE p.neighborho=(%s) AND DATE(requested_datetime) BETWEEN (%s) AND (%s)
         GROUP BY date(r.requested_datetime), r.status order by date(r.requested_datetime) ASC
     """, (neighborhood, fmt_start_date, fmt_end_date))
     
@@ -349,7 +436,7 @@ def daily_count_by_neighborhood(count=None):
     """
     
     return json.dumps(results)
-    
+
 @app.route("/")
 def home(daily_count=None):
     daily_count_res = query_db("""
@@ -403,17 +490,17 @@ def types_list():
 def daily_list():
     return render_template('daily.html')
     
-
+"""
 @app.route("/requests/latest", methods=['POST', 'GET'])
-def get_latest_requests():
+def get_latest_requests1():
     days = request.args.get("days", "60")
     if not days.isdigit():
         raise Exception("'days' must be a number")
 
-    res = query_db("""
+    res = query_db("
         SELECT MAX(requested_datetime) AS max_date
         FROM sf_requests
-    """)
+    ")
     # print 'latest:', res
     end_date = res[0]['max_date']
     # print 'max date:', end_date
@@ -421,6 +508,7 @@ def get_latest_requests():
     start_date = end_date - timedelta(days=int(days))
     fmt = '%Y-%m-%d'
     return get_requests_by_date(start_date.strftime(fmt), end_date.strftime(fmt))
+"""
 
 # Handle dates
 @app.route("/requests/daily/<start_day>..<end_day>", methods=['POST', 'GET'])
@@ -434,15 +522,13 @@ def get_requests_by_date(start_day=None, end_day=None):
         
         res = query_db("""
             SELECT 
-                status, service_name, service_request_id, 
-                CAST(DATE(requested_datetime)AS text) as requested_date, 
-                CAST(DATE(updated_datetime) AS text) as updated_date, 
-                CAST(DATE(expected_datetime) AS text) as expected_date, address, 
-                lat, lon 
+                service_request_id,status, service_code, 
+                CAST(DATE(requested_datetime)AS text) as requested_date,lat, lon 
             FROM sf_requests 
             WHERE requested_datetime BETWEEN (%s) AND (%s) 
             ORDER BY requested_datetime ASC Limit 1000
         """, (start_date, end_date))
+        
         return json.dumps(res)
     else:
         return 'none'
@@ -494,6 +580,8 @@ def request_display_by_date(type=None, start_day=None, end_day=None):
             return render_template('requests_range.html', results=requests_json)
     else:
         return create_null_response()
+
+### STATS ###
         
 @app.route("/avg_resp_time")
 def avg_resp_time():
@@ -519,6 +607,152 @@ def avg_resp_time():
         """)
         
         return json.dumps(res)
+
+### CSV FOR THE DASHBOARD PAGE ###
+import csv
+from cStringIO import StringIO
+def render_csv(rows, columns, format='csv', content_type='text/plain', cors='*'):
+    io = StringIO()
+    if format == 'tsv':
+        dialect = 'excel-tab'
+    else:
+        dialect = 'excel'
+    
+    writer = csv.DictWriter(io, columns, dialect=dialect)
+    writer.writeheader()
+    writer.writerows(rows)
+    response = make_response(io.getvalue(), 200)
+    response.headers['Content-Type'] = content_type
+    if cors is not None:
+        response.headers['Access-Control-Allow-Origin'] = cors
+    return response
+
+@app.route("/requests/latest", methods=['GET'])
+def get_latest_csv():
+    days = request.args.get("days", "20")
+    if not days.isdigit():
+        raise Exception("'days' must be a number")
+
+    res = query_db("""
+        SELECT MAX(requested_datetime) AS max_date
+        FROM sf_requests
+    """)
+    #print 'latest:', res
+    end_date = res[0]['max_date']
+    #print 'max date:', end_date
+
+    start_date = end_date - timedelta(days=int(days))
+    
+    fmt = '%Y-%m-%d'
+    
+    return get_requests_by_date_csv('csv', start_date.strftime(fmt), end_date.strftime(fmt))
+
+@app.route("/requests/reqs.<format>", methods=['GET'])
+def get_requests_by_date_csv(format=None,start_day=None, end_day=None):
+    if start_day is None:
+        start_day = request.args.get('start_day', None)
+        end_day = request.args.get('end_day', None)
+        time_delta = request.args.get('time_delta', None)
+    
+    if format in ('csv','tsv'):
+        if start_day:
+            print start_day, end_day
+            
+            start_date = parse_date(start_day)
+            end_date = (end_day and parse_date(end_day)) or start_date + ONE_DAY
+        elif end_day:
+            end_date = parse_date(end_day)
+            start_date = end_date - timedelta(days=int(time_delta))
+            
+        res = query_db("""
+            SELECT 
+                service_request_id,status,service_code, 
+                CAST(DATE(requested_datetime)AS text) as requested_date,lat,lon 
+            FROM sf_requests 
+            WHERE DATE(requested_datetime) BETWEEN (%s) AND (%s) 
+            ORDER BY requested_datetime DESC Limit 6000
+        """, (start_date, end_date))
+    
+        return render_csv(res, ['service_request_id', 'status','service_code', 'requested_date','lat','lon'])
+    else:
+        return create_null_response()
+
+@app.route("/stats/sr_counts", methods=['GET'])
+def get_sr_counts_by_range():        
+    days = request.args.get("days", "60")
+    neighborhood = request.args.get("neighborhood", None)
+    
+    res = query_db("""
+        SELECT MAX(requested_datetime) AS max_date
+        FROM sf_requests
+    """)
+    
+    end_date = res[0]['max_date']
+
+    start_date = end_date - timedelta(days=int(days))
+    
+    fmt = '%Y-%m-%d'
+    
+    start_day = start_date.strftime(fmt);
+    end_day = end_date.strftime(fmt)
+    
+    if neighborhood:
+        # TODO: Service request counts per neighborhood
+        pass
+    else:
+        res = query_db("""
+            SELECT 
+                CAST(DATE(requested_datetime)AS text) as r_dt, service_code, COUNT(service_code) AS count 
+            FROM sf_requests 
+            WHERE DATE(requested_datetime) BETWEEN (%s) AND (%s) 
+            GROUP by r_dt,service_code
+            ORDER by r_dt DESC
+        """, (start_date, end_date))
+        
+        data = {}
+        
+        service_list = {"1":"Garbage","2":"Sidewalk or Street","3":"Sidewalk or Street",
+        "4":"Garbage","5":"Sidewalk or Street","6":"Trees","7":"Defacement / Graffiti",
+        "8":"Defacement / Graffiti","9":"Defacement / Graffiti","10":"Defacement / Graffiti",
+        "11":"Defacement / Graffiti","12":"Defacement / Graffiti","13":"Defacement / Graffiti",
+        "15":"Garbage","16":"Trees","17":"Sidewalk or Street","18":"Sidewalk or Street",
+        "19":"Garbage","20":"Trees","21":"Sidewalk or Street","22":"Sewage","23":"Sewage",
+        "24":"Sidewalk or Street","25":"Sidewalk or Street","26":"Sidewalk or Street","27":"Trees",
+        "29":"Defacement / Graffiti","30":"Sewage","31":"Sewage","32":"Sewage","33":"Vehicles",
+        "47":"Water","68":"Defacement / Graffiti","172":"Garbage","174":"Garbage","375":"Sidewalk or Street",
+        "376":"Sidewalk or Street","377":"Sidewalk or Street","378":"Sidewalk or Street","379":"Sidewalk or Street"}
+        
+        counts = {}
+        
+        for i, sr_date_count in enumerate(res):
+            #current_date = sr_date_count['r_dt']
+            if sr_date_count['service_code'] in service_list:
+                if sr_date_count['r_dt'] in data:
+                    current_category = service_list[sr_date_count['service_code']]
+                    
+                    if current_category in counts:
+                        counts[current_category] += int(sr_date_count['count'])
+                    else:
+                        counts[current_category] = int(sr_date_count['count'])
+                
+                else:
+                    data[sr_date_count['r_dt']] = {}
+                        
+                    current_category = service_list[sr_date_count['service_code']]
+                    
+                    if current_category in counts:
+                        counts[current_category] += int(sr_date_count['count'])
+                    else:
+                        counts[current_category] = int(sr_date_count['count'])
+            
+                data[sr_date_count['r_dt']] = counts
+            
+            if i < len(res) - 1: 
+                if res[i]['r_dt'] != res[i+1]['r_dt']:
+                    counts = {}
+            
+        return json.dumps(data)
+    
                     
 @app.route("/dashboard/")
 def dashboard():
@@ -534,7 +768,6 @@ if __name__ == "__main__":
     parser.add_option('--port', dest='port', type='int', default=80)
     parser.add_option('--host', dest='host', default="0.0.0.0")
     options, args = parser.parse_args()
-
 
     #app.run()
     app.run(host=options.host, port=options.port)
