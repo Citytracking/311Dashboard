90efc0828e35669f61dcc457ca4c94a5d343d7bf
Michael Lawrence Evans <mevans@stamen.com>
2012-08-24 17:05:04 -0700

    Adding service list

---
+++ flask/static/new_service_list.js
@@ -0,0 +1,162 @@
+var new_service_list = {
+  "1": {
+    "group": "Garbage", 
+    "service_name": "Cans Left Out 24x7"
+  }, 
+  "2": {
+    "group": "Sidewalk or Street", 
+    "service_name": "Construction Plate Shifted"
+  }, 
+  "3": {
+    "group": "Sidewalk or Street", 
+    "service_name": "Curb or Curb Ramp Defect"
+  }, 
+  "4": {
+    "group": "Garbage", 
+    "service_name": "Damaged City Can"
+  }, 
+  "5": {
+    "group": "Sidewalk or Street", 
+    "service_name": "Damaged Side Sewer Vent Cover"
+  }, 
+  "6": {
+    "group": "Trees", 
+    "service_name": "Trees - Damaged Tree"
+  }, 
+  "7": {
+    "group": "Defacement / Graffiti", 
+    "service_name": "Graffiti - Public - Bike rack"
+  }, 
+  "8": {
+    "group": "Defacement / Graffiti", 
+    "service_name": "Graffiti - Public - News rack"
+  }, 
+  "9": {
+    "group": "Defacement / Graffiti", 
+    "service_name": "Graffiti - Public - Parking meter"
+  }, 
+  "10": {
+    "group": "Defacement / Graffiti", 
+    "service_name": "Graffiti - Public - Pay phone"
+  }, 
+  "11": {
+    "group": "Defacement / Graffiti", 
+    "service_name": "Graffiti - Private - Not Offensive"
+  }, 
+  "12": {
+    "group": "Defacement / Graffiti", 
+    "service_name": "Graffiti - Private - Offensive"
+  }, 
+  "13": {
+    "group": "Defacement / Graffiti", 
+    "service_name": "Graffiti - Public - Other enter additional details below"
+  }, 
+  "15": {
+    "group": "Garbage", 
+    "service_name": "Illegal Dumping"
+  }, 
+  "16": {
+    "group": "Trees", 
+    "service_name": "Trees - Landscaping Request"
+  }, 
+  "17": {
+    "group": "Sidewalk or Street", 
+    "service_name": "Missed Route Mechanical Sweeping"
+  }, 
+  "18": {
+    "group": "Sidewalk or Street", 
+    "service_name": "Missing Side Sewer Vent Cover"
+  }, 
+  "19": {
+    "group": "Garbage", 
+    "service_name": "Overflowing City Receptacle or Dumpster"
+  }, 
+  "20": {
+    "group": "Trees", 
+    "service_name": "Trees - Overgrown Tree"
+  }, 
+  "21": {
+    "group": "Sidewalk or Street", 
+    "service_name": "Pavement Defect"
+  }, 
+  "22": {
+    "group": "Sewage", 
+    "service_name": "Sewage Back Up Out of catch basin"
+  }, 
+  "23": {
+    "group": "Sewage", 
+    "service_name": "Side Sewer issues Water coming out"
+  }, 
+  "24": {
+    "group": "Sidewalk or Street", 
+    "service_name": "Sidewalk Cleaning"
+  }, 
+  "25": {
+    "group": "Sidewalk or Street", 
+    "service_name": "Sidewalk Defect"
+  }, 
+  "26": {
+    "group": "Sidewalk or Street", 
+    "service_name": "Street Cleaning"
+  }, 
+  "27": {
+    "group": "Trees", 
+    "service_name": "Trees - Damaging Property"
+  }, 
+  "29": {
+    "group": "Defacement / Graffiti", 
+    "service_name": "Sign - Defaced"
+  }, 
+  "30": {
+    "group": "Sewage", 
+    "service_name": "Side Sewer issues Odor"
+  }, 
+  "31": {
+    "group": "Sewage", 
+    "service_name": "Sewage Back up Out of manhole cover"
+  }, 
+  "32": {
+    "group": "Sewage", 
+    "service_name": "Sewage Back up Out of sewer vent 4 inch"
+  },
+    "33": {
+    "group": "Vehicles", 
+    "service_name": "Abandoned Vehicle"
+  },
+  "47": {
+    "group": "Water", 
+    "service_name": "Flooding"
+  },
+  "68": {
+    "group": "Defacement / Graffiti", 
+    "service_name": "Illegal Posting"
+  }, 
+  "172": {
+    "group": "Garbage", 
+    "service_name": "Garbage and/or overgrowth on vacant lot"
+  }, 
+  "174": {
+    "group": "Garbage", 
+    "service_name": "Garbage and/or overgrowth near building"
+  }, 
+  "375": {
+    "group": "Sidewalk or Street", 
+    "service_name": "Damaged Benches on Sidewalk"
+  }, 
+  "376": {
+    "group": "Sidewalk or Street", 
+    "service_name": "Damaged Bike Rack"
+  }, 
+  "377": {
+    "group": "Sidewalk or Street", 
+    "service_name": "Damaged Kiosk or Public Toilet"
+  }, 
+  "378": {
+    "group": "Sidewalk or Street", 
+    "service_name": "Damaged News Rack"
+  }, 
+  "379": {
+    "group": "Sidewalk or Street", 
+    "service_name": "Damaged Parking Meter"
+  }
+}
